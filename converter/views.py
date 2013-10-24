887ea4bc95743794832bc6b1f149d3f604faf929
Matt Harrison <matt.harrison91@gmail.com>
2013-10-24 15:23:18 +0100

    Include basic google maps support

--- converter/views.py
+++ converter/views.py
@@ -1,11 +1,16 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader, RequestContext
 
 def index(request):
     return render(request, 'converter/index.html')
 
 def map(request):
-    input = request.POST['coordinate']
-    return render(request, 'converter/map.html')
+    lat = request.POST['lat']
+    long = request.POST['long']
+    template = loader.get_template('converter/map.html')
+    context = RequestContext(request, {'lat': lat, 'long': long})
+    return HttpResponse(template.render(context))
+
 

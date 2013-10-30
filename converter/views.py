9d7da209c84895b26c22735e54681f64da4bdc34
Matt Harrison <matt.harrison91@gmail.com>
2013-10-30 13:52:58 +0000

    Import code from transformation, fully implement all 3 data input types and properly display on Gmaps

--- converter/views.py
+++ converter/views.py
@@ -2,15 +2,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader, RequestContext
+from transformation.OSTN02 import webgui_convert
 
 def index(request):
     return render(request, 'converter/index.html')
 
 def map(request):
-    lat = request.POST['lat']
-    long = request.POST['long']
+    system = request.POST['system']
+    lat = float(request.POST['1'])
+    lng = float(request.POST['4'])
+    if system == 'NE':
+        lat,lng = webgui_convert(float(request.POST['1']), float(request.POST['4']))
+    if system == 'DMS':
+        lat = float(request.POST['1']) + (float(request.POST['2'])/60) + (float(request.POST['3'])/360)
+        lng = float(request.POST['4']) + (float(request.POST['5']) / 60) + (float(request.POST['6']) / 360)
+
     template = loader.get_template('converter/map.html')
-    context = RequestContext(request, {'lat': lat, 'long': long})
+    context = RequestContext(request, {'lat': lat, 'long': lng, 'system': system})
     return HttpResponse(template.render(context))
 
 

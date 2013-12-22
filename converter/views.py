5918885bf71b633311c689921983aed2322e2888
Matt Harrison <matt.harrison91@gmail.com>
2013-12-22 21:02:43 +0000

    Include support for string labelled points

--- converter/views.py
+++ converter/views.py
@@ -10,22 +10,21 @@ from transformation.transformation.OSTN02 import webgui_convert
 
 
 def handle_uploaded_file(f):
-
-    i=0
+    i = 0
     csvin = csv.reader(f)
     row = list(csvin)
     totalrows = len(row)
-    points = numpy.zeros((totalrows,4 ))
+    label = []
+    points = numpy.zeros((totalrows, 3 ))
     csvin = csv.reader(f)
     for row in csvin:
+        points[i][0] = row[1]
+        points[i][1] = row[2]
+        points[i][2] = row[3]
+        label.append(row[0])
+        i += 1
 
-        points[i][0] = row[0]
-        points[i][1] = row[1]
-        points[i][2] = row[2]
-        points[i][3] = row[3]
-        i = i+1
-
-    return points, totalrows
+    return points, totalrows, label
 
 #with open('c:\users\matt\projects\webgui\data.txt', 'wb+') as destination:
 #  for chunk in f.chunks():
@@ -43,16 +42,14 @@ def map(request):
     if "convert" in request.POST.keys():
         convert = True
     if "usefile" in request.POST.keys():
-        points, totalrows = handle_uploaded_file(request.FILES['path'])
+        points, totalrows, label = handle_uploaded_file(request.FILES['path'])
         print(points)
         lat = numpy.zeros(totalrows)
         lng = numpy.zeros(totalrows)
         height = numpy.zeros(totalrows)
-        label = numpy.zeros(totalrows)
         for i in range(0, totalrows, 1):
-            lat[i], lng[i], height[i] = webgui_convert(points[i][1], points[i][2], points[i][3], convert)
-            label[i] = points[i][0]
-        print(lat,lng,height)
+            lat[i], lng[i], height[i] = webgui_convert(points[i][0], points[i][1], points[i][2], convert)
+        print(lat, lng, height)
         print(zip(lat, lng, label))
     else:
         lat = numpy.zeros(1)
@@ -61,7 +58,7 @@ def map(request):
         label = 'Point'
         if system == 'NE':
             lat[0], lng[0], height[0] = webgui_convert(float(request.POST['east']), float(request.POST['north']),
-                                              float(request.POST['height']), convert)
+                                                       float(request.POST['height']), convert)
             print(lat, lng, height)
         elif system == 'DMS':
             lat[0] = float(request.POST['latd']) + (float(request.POST['latm']) / 60) + (
@@ -75,9 +72,10 @@ def map(request):
             lng[0] = float(request.POST['lngdd'])
             height[0] = float(request.POST['height'])
 
-
     template = loader.get_template('converter/map.html')
-    context = RequestContext(request, {'latlist': lat, 'longlist': lng, 'system': system, 'totalrows': xrange(totalrows), 'lat_and_long_label': zip(lat, lng, label)})
+    context = RequestContext(request,
+                             {'latlist': lat, 'longlist': lng, 'system': system, 'totalrows': xrange(totalrows),
+                              'lat_and_long_label': zip(lat, lng, label)})
     return HttpResponse(template.render(context))
 
 
